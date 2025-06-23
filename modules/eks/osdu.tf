fdf09609a84916414283199deb1491d9aabfd957
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-06-23 16:08:23 +0530

    UPDATED THE TERRAFORM CODE

--- modules/eks/osdu.tf
+++ modules/eks/osdu.tf
@@ -242,29 +242,6 @@ locals {
   istio_hostname = try(data.kubernetes_service.istio_ingress.status.0.load_balancer.0.ingress.0.hostname, "")
 }
 
-# # OSDU Baremetal Helm Installation
-# resource "helm_release" "osdu_baremetal" {
-#   name       = "osdu-baremetal"
-#   repository = "oci://community.opengroup.org:5555/osdu/platform/deployment-and-operations/infra-gcp-provisioning/gc-helm"
-#   chart      = "osdu-gc-baremetal"
-#   namespace  = "default"
-#   timeout    = 1800  # 30 minutes
-#   wait       = true
-
-#   # This reads your custom-values.yaml file
-#   # Use the custom-values.yaml file from the same directory as this tf file
-#   values = [
-#     file("${path.module}/custom-values.yaml")
-#   ]
-
-#   depends_on = [
-#     data.kubernetes_service.istio_ingress
-#   ]
-# }
-
-
-
-
 # OSDU Baremetal Helm Installation
 resource "helm_release" "osdu_baremetal" {
   name       = "osdu-baremetal"
@@ -274,18 +251,8 @@ resource "helm_release" "osdu_baremetal" {
   timeout    = 1800  # 30 minutes
   wait       = true
 
-  # Force update configuration
-  force_update    = true
-  recreate_pods   = true
-  cleanup_on_fail = true
-  atomic          = true
-  
-  # Replace trigger to force upgrades when values change
-  # replace_triggered_by = [
-  #   filesha256("${path.module}/custom-values.yaml")
-  # ]
-
   # This reads your custom-values.yaml file
+  # Use the custom-values.yaml file from the same directory as this tf file
   values = [
     file("${path.module}/custom-values.yaml")
   ]
@@ -294,3 +261,36 @@ resource "helm_release" "osdu_baremetal" {
     data.kubernetes_service.istio_ingress
   ]
 }
+
+
+
+
+# # OSDU Baremetal Helm Installation
+# resource "helm_release" "osdu_baremetal" {
+#   name       = "osdu-baremetal"
+#   repository = "oci://community.opengroup.org:5555/osdu/platform/deployment-and-operations/infra-gcp-provisioning/gc-helm"
+#   chart      = "osdu-gc-baremetal"
+#   namespace  = "default"
+#   timeout    = 1800  # 30 minutes
+#   wait       = true
+
+#   # Force update configuration
+#   force_update    = true
+#   recreate_pods   = true
+#   cleanup_on_fail = true
+#   atomic          = true
+  
+#   # Replace trigger to force upgrades when values change
+#   # replace_triggered_by = [
+#   #   filesha256("${path.module}/custom-values.yaml")
+#   # ]
+
+#   # This reads your custom-values.yaml file
+#   values = [
+#     file("${path.module}/custom-values.yaml")
+#   ]
+
+#   depends_on = [
+#     data.kubernetes_service.istio_ingress
+#   ]
+# }
