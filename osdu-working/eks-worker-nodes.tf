2b419fa02db1f9593bcf343520f088da59954d7c
karthikpdm <karthikbm.pdm19@rvce.edu.in>
2025-06-30 20:20:14 +0530

    UPDATED THE TERRAFORM CODE

--- osdu-working/eks-worker-nodes.tf
+++ osdu-working/eks-worker-nodes.tf
@@ -151,9 +151,9 @@ resource "aws_eks_node_group" "osdu_ir_frontend_node" {
   ]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 1
-    min_size     = 1
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
   }
 
   instance_types = [var.instance_type]
